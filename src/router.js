078797ccd4b14cb8e11cb891b81d333567d62652
zahra-ove <zahra.oveyedzade@gmail.com>
2024-12-08 17:00:50 +0330

    Add routes

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', components: null },
+        { path: '/coaches/:id', component: null, children: [
+                { path: 'contact', component: null },
+            ]},
+        { path: '/register', component: null },
+        { path: '/requests', component: null },
+        { path: '/:notFound(.*)', component: null },
+    ]
+});
+
+
+export default router;
