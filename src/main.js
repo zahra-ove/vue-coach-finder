debf72d6db7c1fc97d60dc90de9e8a430c7ece95
zahra-ove <zahra.oveyedzade@gmail.com>
2024-12-08 19:37:13 +0330

    Add store by Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import router from './router.js';
+import store from './store/index.js';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,10 +5,24 @@
       <button>Refresh</button>
       <router-link to="/register">Register a Coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{coach.firstName}} {{coach.lastName}}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+}
+</script>
