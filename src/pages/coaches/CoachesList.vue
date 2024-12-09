bbfc522b9fdd8ba7651d8d4cec0e8c6b522724ab
zahra-ove <zahra.oveyedzade@gmail.com>
2024-12-09 10:23:53 +0330

    Add base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,34 @@
 <template>
   <section>FILTER</section>
   <section>
+    <base-card>
     <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register a Coach</router-link>
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register">Register a Coach</base-button>
     </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{coach.firstName}} {{coach.lastName}}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filteredCoaches"
+                    :key="coach.id"
+                    :id="coach.id"
+                    :first-name="coach.firstName"
+                    :last-name="coach.lastName"
+                    :rate="coach.hourlyRate"
+                    :areas="coach.areas"
+        >
+        </coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
+import CoachItem from "@/components/coaches/CoachItem.vue";
+
 export default {
+  components: {
+    CoachItem
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +39,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import { createStore} from 'vuex';
+import { createStore } from 'vuex';
 
-import coachesModule from './modules/coaches/index';
+import coachesModule from './modules/coaches/index.js';
 
 const store = createStore({
     modules: {
